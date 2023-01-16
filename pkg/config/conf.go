fb39a3e9a39b233ef8847dfd08c37b8dcc9d30a1
emilbui <frostwyrm9x@gmail.com>
2023-01-16 14:18:44 +0700

    update README

--- pkg/config/conf.go
+++ pkg/config/conf.go
@@ -5,6 +5,7 @@ import "github.com/spf13/viper"
 func Get(relativePath string) *viper.Viper {
 	config := viper.New()
 	config.SetConfigFile(relativePath)
+	config.AutomaticEnv()
 	err := config.ReadInConfig()
 	if err != nil {
 		panic(err)
